6a103f617d6c1b0b12cf02fdcae14abb544789ed
pnchoti <jgchotirat@gmail.com>
2023-08-16 15:17:20 +0200

    Create Dictionary.js

---
+++ src/Dictionary.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Dictionary() {
+    return (
+        <div className="Dictionary">
+            <h1> English Dictionary </h1>
+
+        </div>
+    );
+}
+
+export default Dictionary;
