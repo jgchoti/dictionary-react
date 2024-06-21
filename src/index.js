e45a052b00097af58b19c69a7a58b6dc5d65f795
jgchoti <jgchotirat@gmail.com>
2024-06-21 16:36:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Footer from "./Footer"
+import Footer from './Footer';
 
-import "bootstrap/dist/css/bootstrap.css";
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
